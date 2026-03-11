d739c3cc8c2c290c584d254ef4362c3338039f6f
vighnesh-gholap <vighneshgholap3856@gmail.com>
2026-03-11 23:08:13 +0530

    Updated attendance system with percentage and storage

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
+let students = JSON.parse(localStorage.getItem("students")) || [];
+
+function saveData(){
+localStorage.setItem("students", JSON.stringify(students));
+}
+
 function addStudent(){
 
-let name=document.getElementById("name").value;
+let name = document.getElementById("name").value;
 
 if(name==""){
 alert("Enter student name");
 return;
 }
 
-let table=document.getElementById("studentList");
+let student = {
+name:name,
+present:0,
+absent:0
+};
 
-let row=table.insertRow();
-
-let cell1=row.insertCell(0);
-let cell2=row.insertCell(1);
-
-cell1.innerHTML=name;
-cell2.innerHTML='<input type="checkbox">';
+students.push(student);
+saveData();
+displayStudents();
 
 document.getElementById("name").value="";
 }
+
+function markAttendance(index,type){
+
+if(type=="present"){
+students[index].present++;
+}
+
+if(type=="absent"){
+students[index].absent++;
+}
+
+saveData();
+displayStudents();
+
+}
+
+function displayStudents(){
+
+let table="";
+
+students.forEach((s,index)=>{
+
+let total = s.present + s.absent;
+let percent = total==0 ? 0 : ((s.present/total)*100).toFixed(1);
+
+table+=`
+<tr>
+<td>${s.name}</td>
+
+<td>
+<button onclick="markAttendance(${index},'present')">Present</button>
+</td>
+
+<td>
+<button onclick="markAttendance(${index},'absent')">Absent</button>
+</td>
+
+<td>${percent}%</td>
+
+</tr>
+`;
+
+});
+
+document.getElementById("studentList").innerHTML = table;
+
+}
+
+displayStudents();
